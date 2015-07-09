ec451eeb7195bedcab64cefc560d5bdee1d7e9a3
gel <corp.gel@gmail.com>
2015-07-09 17:48:21 -0500

    Nuevo Apartado Author

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz' });
 });
-
+router.get('/author',function(req,res){
+    res.render('author',{title:'Creditos'})
+});
 module.exports = router;
