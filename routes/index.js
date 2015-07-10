9cb28acd5d4c76eb1e6dc093950453cf62fcace1
gel <corp.gel@gmail.com>
2015-07-09 20:51:24 -0500

    respaldo 2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz Curso MiriadaX' });
 });
 router.get('/author',function(req,res){
     res.render('author',{title:'Creditos'})
